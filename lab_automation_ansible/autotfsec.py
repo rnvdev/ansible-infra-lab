8db3693e1a648a0be1b7fad5d06cb6d58224c09b
Raphael Viana <123553646+rnvdev@users.noreply.github.com>
2023-08-22 16:15:53 -0300

    Update autotfsec.py

--- lab_automation_ansible/autotfsec.py
+++ lab_automation_ansible/autotfsec.py
@@ -2,10 +2,17 @@ import os
 
 dirs = os.listdir("./")
 command = "tfsec "
-avoid_list = ["autotfsec.py", "scripts", "main.tf:Zone.Identifier", "main.tf"]
+avoid_list = ["autotfsec.py", "scripts", "main.tf:Zone.Identifier", "main.tf","output.txt"]
+scan_result = []
 
 for dir in dirs:
     if dir in avoid_list:
         continue
     print(f"SCANNING-------> {dir} <-----")
-    os.system("tfsec " + dir + "/")
+    os.system("tfsec " + dir + "/ > output.txt")
+
+    with open("output.txt", "r") as file:
+        last_line = file.readlines()[-3]
+        scan_result.append(last_line)
+        print(f"AQUI: {last_line}")
+        
